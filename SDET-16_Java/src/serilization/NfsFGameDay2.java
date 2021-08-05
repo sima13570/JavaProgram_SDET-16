b860cfac88b601a1a11ff978ccc5daa6121ed57b
SEEMA KUMARI <SEEMA KUMARI@DESKTOP-FR4KE14>
2021-08-05 12:27:16 +0530

    push

--- SDET-16_Java/src/serilization/NfsFGameDay2.java
+++ SDET-16_Java/src/serilization/NfsFGameDay2.java
@@ -1,8 +1,6 @@
 package serilization;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 
